04d8c5003c1971619e13eeaf0329b75f411de966
Matt Takao <takaomatt@gmail.com>
2021-05-23 15:38:54 -0700

    2.1.3

--- lib/constants/keys.dart
+++ lib/constants/keys.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 // debug mode - FALSE for production
-const bool debugModeEnabled = true;
+const bool debugModeEnabled = false;
 const int debugTestTime = 30;
 
 const String usernameKey = 'Username';
